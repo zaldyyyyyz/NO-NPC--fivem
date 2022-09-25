ad09e83b6986ddafcf8625019010084a8da6de57
zaldyyyyyz <114321500+zaldyyyyyz@users.noreply.github.com>
2022-09-25 10:17:25 +0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+
+game 'gta5'
+author 'zaldy'
+description 'No NPC'
+version '1.1.3'
+
+client_script 'main.lua'
+
+server_script 'server.lua'
---
+++ main.lua
@@ -0,0 +1,41 @@
+--
+--__    __   ______         __    __  _______    ______  
+--|  \  |  \ /      \       |  \  |  \|       \  /      \ 
+--| $$\ | $$|  $$$$$$\      | $$\ | $$| $$$$$$$\|  $$$$$$\
+--| $$$\| $$| $$  | $$      | $$$\| $$| $$__/ $$| $$   \$$
+--| $$$$\ $$| $$  | $$      | $$$$\ $$| $$    $$| $$      
+--| $$\$$ $$| $$  | $$      | $$\$$ $$| $$$$$$$ | $$   __ 
+--| $$ \$$$$| $$__/ $$      | $$ \$$$$| $$      | $$__/  \
+--| $$  \$$$ \$$    $$      | $$  \$$$| $$       \$$    $$
+-- \$$   \$$  \$$$$$$        \$$   \$$ \$$        \$$$$$$ 
+-------------------------------------------------------------
+-------------------------------------------------------------
+-------------------------------------------------------------
+
+
+---CONFIG FOR NPC OR AI 
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0) -- Prevent crashing.
+
+		-- Stop Spawn
+		SetCreateRandomCops(false)
+		SetCreateRandomCopsNotOnScenarios(false)
+		SetCreateRandomCopsOnScenarios(false)
+		SetGarbageTrucks(false)
+		SetRandomBoats(false)
+       	SetVehicleDensityMultiplierThisFrame(0.0)
+       	SetPedDensityMultiplierThisFrame(0.0)
+		SetRandomVehicleDensityMultiplierThisFrame(0.0)
+		SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
+		SetParkedVehicleDensityMultiplierThisFrame(0.0)
+
+		-- Clear NPC
+		local x,y,z = table.unpack(GetEntityCoords(PlayerPedId()))
+		ClearAreaOfVehicles(x, y, z, 1000, false, false, false, false, false)
+		RemoveVehiclesFromGeneratorsInArea(x - 500.0, y - 500.0, z - 500.0, x + 500.0, y + 500.0, z + 500.0);
+    end
+end)
+
+
+-- SERVER MAIN IS PREVENT FOR CRASHING AND OPTIMIZE SIDE CHICK Nigga --- 
